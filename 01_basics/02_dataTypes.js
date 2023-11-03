1305c6f53e5240100374a2dee41341d85e87c254
akanksha_V_2599 <vish.akanksha099@gmail.com>
2023-11-03 09:17:41 +0530

    Datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using node.js not browser
+
+console.log(3
+    +
+    3) // code readability must be high**
+
+console.log("Akanksha");
+
+let name = "Akanksha";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// primitive datatypes
+
+// Number => 2 to the power of 53
+// BigInt => used for big number genrally in stock market and trading Websites
+// String =>  ""
+// Boolean => true/false
+// null => standalone value
+// undefined => value not assigned to variable
+// symbol => unique
+
+
+console.log(typeof "Akanksha");
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
