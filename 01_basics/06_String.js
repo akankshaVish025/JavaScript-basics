a14b5ba932171c4354fd82cff01b28ba8a41d516
akanksha-V-2599 <vish.akanksha099@gmail.com>
2024-07-29 19:02:27 +0530

    new tests

--- 01_basics/06_String.js
+++ 01_basics/06_String.js
@@ -4,7 +4,7 @@ console.log(name + repoCount + " Value");
 
 // String Interpolation syntax: ${...}
 /** 
-1. string interpolation is the process of replacing placeholders with values in a string literal
+1. string interpolation( ${} ) is the process of replacing placeholders with values in a string literal
 2. It is done using back-ticks (``).
 **/
 
@@ -41,3 +41,63 @@ console.log(url.includes('hitesh'), "Includes method");
 console.log(url.includes('sundar'), "Includes method");
 
 console.log(gameName.split('-'), "Split method");
+
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+let index1 = text.lastIndexOf("locate");
+console.log(index, index1, "index");
+let sr = text.search("locate");    // OR text.search(/locate/);   both will give same output
+console.log(sr, "Search");
+
+/** Note: Even if indexOf() and search() gives same output both methods are NOT EQUAL
+
+1. indexOf() Method
+Purpose: Finds the first occurrence of a specified value within a string.
+Arguments:
+searchValue (required): The string to search for.
+startIndex (optional): The position within the string to start the search. Defaults to 0.
+
+2. search() Method
+Purpose: Searches for a match between a regular expression and a string.
+Arguments:
+regexp (required): The regular expression to search for.
+
+Differences:
+
+Second Argument:
+indexOf(): Accepts a second optional argument startIndex to specify the position within the string to start the search.
+search(): Does not accept a second argument.
+
+Search Values:
+indexOf(): Can only search for a simple substring.
+search(): Can use regular expressions, which provide powerful search capabilities.
+
+*/
+
+// match() method
+
+//Perform a search for "ain":
+
+let text1 = "The rain in SPAIN stays mainly in the plain";
+console.log(text1.match("ain"))
+
+console.log(text1.match(/ain/));
+
+// Perform a global search for "ain":
+
+console.log(text1.match(/ain/g));
+ 
+// Perform a global, case-insensitive search for "ain":
+
+console.log(text1.match(/ain/gi));
+
+/** Note: 
+If a regular expression does not include the g modifier (global search), 
+match() will return only the first match in the string.
+*/
+
+// matchAll()
+
+let text2 = "I love cats. Cats are very easy to love. Cats are very popular."
+const iterator = text.matchAll("Cats")
+
---
+++ 01_basics/07_num&maths.js
@@ -0,0 +1,12 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString(), "toString()")
+
+const newNumber = 23.890;
+console.log(newNumber.toFixed(1), "toFixed() to 1 value after decimal");
+
+console.log(newNumber.toPrecision(3));
