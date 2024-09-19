3b9583988f7b26a4e8ea6b8856fd27d3728958a9
akanksha-V-2599 <vish.akanksha099@gmail.com>
2024-09-19 18:55:38 +1300

    Add new changes

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -8,5 +8,22 @@ Objects are created using 2 ways:
 // Object.create()
 
 // Object Literals:
-const JsUser = {}
+const JsUser = {
+    name: "Akanksha",
+    age: 18,
+    "full name": "Akanksha Vishwakarma",
+    location: "Jaipur",
+    email: "akshu@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}                                                                        // this is how object is created
+
+// To access JS object properties
+console.log(JsUser.email, "Using dot");
+console.log(JsUser["email"], "Using square bracket");
+console.log(JsUser["full name"]);   //properties like("full name") are always accessed using square bracket
+
+// Note: JsUser["email"]
+// when using square bracket always write property name in double quotes("")
+
 
